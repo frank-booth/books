8a1b19b58e69deb6f34ec8857354160738d21fbd
frank-booth <jfraley519@gmail.com>
2023-01-23 12:57:53 -0500

    added CRUD

--- books/src/App.js
+++ books/src/App.js
@@ -13,15 +13,27 @@ function App() {
     setBooks(updatedBooks)
   }
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle }
+      }
+      return book
+    })
+    setBooks(updatedBooks)
+  }
+
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
       return book.id !== id
     })
     setBooks(updatedBooks)
   }
+
   return (
     <div className="app">
-      <BookList books={books} onDelete={deleteBookById} />
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   )
