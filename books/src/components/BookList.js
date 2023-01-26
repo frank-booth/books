43792de7d8eb0f6261a3aa490c615567688cc6ed
frank-booth <jfraley519@gmail.com>
2023-01-26 12:53:42 -0500

    refactored using context

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,21 +1,14 @@
-import BookShow from './BookShow'
 import { useContext } from 'react'
 import BooksContext from '../context/books'
+import BookShow from './BookShow'
 
-function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BooksContext)
+function BookList() {
+  const { books } = useContext(BooksContext)
+
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
-    )
+    return <BookShow key={book.id} book={book} />
   })
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click</button>
-      {renderedBooks}
-    </div>
-  )
+  return <div className="book-list">{renderedBooks}</div>
 }
 
 export default BookList
